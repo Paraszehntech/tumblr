87565185f99e698b6fbd4f3cff7f460d51fce9a0
Paraszehntech <parasyadav8989@gmail.com>
2024-02-19 18:49:42 +0530

    add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    
+	def create
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment].permit(:name, :body))
+
+		redirect_to post_path(@post)
+	end
+
 end
